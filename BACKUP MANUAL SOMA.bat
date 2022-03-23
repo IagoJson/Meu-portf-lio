b30dfbc3c44506ae09b2f33c0baf6fba169ea19b
Iago Beckedorff <102250730+IagoJson@users.noreply.github.com>
2022-03-23 17:02:33 -0300

    Finalizado
    Codigo .bat para realizar o backup de um arquivo onde ele move para uma outra pasta(sendo até por uma rede) onde ele cria uma nova pasta organizando por ano, mês e data.

---
+++ BACKUP MANUAL SOMA.bat
@@ -0,0 +1,32 @@
+@echo off 
+color a
+
+@echo COPIANDO ARQUIVO DE BACKUP DO SOMA...
+
+IF EXIST R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4% (
+
+
+	IF EXIST R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2% (
+		md R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2%\%date:~0,2%
+		copy "C:\Arquivos de Programas\tarifador\lig.txt" "R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2%\%date:~0,2%\"	
+ 
+
+	) ELSE (
+		md R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2%
+		md R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2%\%date:~0,2%
+		copy "C:\Arquivos de Programas\tarifador\lig.txt" "R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2%\%date:~0,2%\"
+)
+) ELSE ( 
+		md R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%
+		md R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2%
+		md R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2%\%date:~0,2%
+		copy "C:\Arquivos de Programas\tarifador\lig.txt" "R:\Grupos\Administrativo\Recepcao\SOMABackup\%date:~6,4%\%date:~3,2%\%date:~0,2%\"
+)
+
+
+@echo By IAGO BECKEDORFF
+@echo Programa desenvolvido para SISTEMA SOMA DE TARIFACAO
+@echo dentro do escritorio LEMOS E ASSOCIADOS ADVOCACIA
+
+
+pause
